b0fe8341c712b0e15ae05cc5df7bc7e7d2c197f0
Devansh <121853355+Devansh29092004@users.noreply.github.com>
2025-05-23 22:00:00 +0530

    here is the 2nd commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,65 @@
 import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToPastes, updateToPastes } from '../redux/pasteSlice';
+
 const Home = () => {
   const [title, setTitle] = useState('');
   const [value, setValue] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
   const pasteId = searchParams.get('pasteId');
+  const dispatch = useDispatch();
+  function createPaste() {
+    const paste = {
+      title: title,
+      content: value,
+      _id: pasteId ||
+        Date.now().toString(36),
+      createAt: new Date().toISOString()
+    };
+
+    if (pasteId) {
+      dispatch(updateToPastes(paste));
+
+    }
+    else {
+      dispatch(addToPastes(paste));
+    }
+
+    setTitle('');
+    setValue('');
+    setSearchParams({});
+
+  }
 
   return (
-    <div className='flex flex-row gap-7'>
-      <input
-        className='p-2 rounded-2xl mt-2'
-        type="text"
-        placeholder="Enter Title Here"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-
-      <button className='p-2 rounded-2xl mt-2'>
-       {
-        pasteId ? "Update My Paste" : "Create My Paste"
-       }
-      </button>
+    <div className="max-w-2xl mx-auto mt-2 p-6">
+      <form>
+        <div className="flex flex-row gap-4 items-center">
+          <input
+            className="flex-1 p-3 rounded-2xl w-[400px] md:w-[600px]"
+            type="text"
+            placeholder="Enter Title Here"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button
+            onClick={createPaste}
+            type="button"
+            className="px-6 py-3 rounded-2xl"
+          >
+            {pasteId ? "Update My Paste" : "Create My Paste"}
+          </button>
+        </div>
+        <div className="mt-8">
+          <textarea
+            className="w-full min-h-[300px] p-4 text-lg rounded-2xl min-w-[400px] md:min-w-[600px]"
+            placeholder="Enter Content Here"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/redux/pasteSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+import toast from 'react-hot-toast';
+const initialState = {
+    pastes:localStorage.getItem('pastes') ? JSON.parse(localStorage.getItem('pastes')) : [],
+};
+
+export const pasteSlice = createSlice({
+    name: 'paste',
+    initialState,
+    reducers: {
+        addToPastes(state, action) {
+           const paste = action.payload;
+           state.pastes.push(paste);
+           localStorage.setItem('pastes', JSON.stringify(state.pastes));
+           toast("Paste Created Successfully!");
+        },
+        updateToPastes(state, action) {
+          const idx = state.pastes.findIndex(p => p._id === action.payload._id);
+          if (idx !== -1) {
+            state.pastes[idx] = action.payload;
+            localStorage.setItem('pastes', JSON.stringify(state.pastes));
+            toast.success("Paste Updated Successfully!");
+          }
+        },
+        resetAllPastes(state) {
+           state.pastes = [];
+           localStorage.removeItem('pastes');
+           toast.success("All pastes removed successfully!");
+        },
+        removeFromPastes(state, action) {
+          const idToRemove = action.payload;
+          state.pastes = state.pastes.filter(paste => paste._id !== idToRemove);
+          localStorage.setItem('pastes', JSON.stringify(state.pastes));
+          toast.success("Paste Removed Successfully!");
+        },
+    },
+});
+
+export const {
+    addToPastes,
+    updateToPastes,
+    resetAllPastes,
+    removeFromPastes,
+} = pasteSlice.actions;
+
+export default pasteSlice.reducer;
